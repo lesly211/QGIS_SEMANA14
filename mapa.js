cbab8c2cedf95e2020c48f7d284f3f1f7f7337f2
Lesly Brenda Navarro Serva <leslynavarro804@gmail.com>
2026-07-06 12:08:11 -0500

    js

--- mapa.js
+++ mapa.js
@@ -15,3 +15,31 @@ var marcadorParque = L.marker([-12.0725, -75.2098]).bindPopup('<b>Parque de la I
 marcadorUNCP.addTo(mapa);
 marcadorPlaza.addTo(mapa);
 marcadorParque.addTo(mapa);
+// 5. Conexión a nuestro servidor local GeoServer (Servicio WMS)
+var capaWMS = L.tileLayer.wms('http://localhost:8080/geoserver/semana12/wms', {
+    layers: 'semana12:Distrito_INEI_2017', // ¡Ojo! Poner el nombre exacto de la capa a usar
+    format: 'image/png',
+    transparent: true,
+    version: '1.1.0',
+    attribution: "Datos del INEI servidos por GeoServer"
+});
+
+// 6. Agregamos la capa WMS al mapa
+capaWMS.addTo(mapa);
+// 7. Configurar el Control de Capas (Layers Control)
+
+// A. Agrupamos los mapas base (Solo se puede ver uno a la vez - Radio buttons)
+var mapasBase = {
+    "Mapa(OSM)": capaOSM
+};
+
+// B. Agrupamos las capas superpuestas (Se pueden ver varias a la vez - Checkboxes)
+var capasSuperpuestas = {
+    "Universidad (UNCP)": marcadorUNCP,
+    "Plaza Constitución": marcadorPlaza,
+    "Parque Identidad": marcadorParque,
+    "Capa Distritos (GeoServer)": capaWMS
+};
+
+// C. Inyectamos el control de capas en la esquina superior derecha del mapa
+L.control.layers(mapasBase, capasSuperpuestas).addTo(mapa);
